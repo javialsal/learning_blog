0295cad43bbafd6d4c31680d520fe18571db214e
Javier Alvarez <javialsal@gmail.com>
2023-10-18 11:26:18 +0200

    Seed modified to create 100 Blog Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Launching seed..."
+puts "Creating admin user..."
 
 user = User.where(email: "javier@mail.com").first_or_initialize
 user.update!(
   password: "password",
   password_confirmation: "password"
 )
+
+puts "Admin user created."
+puts "Deleting Blog Posts..."
+
+BlogPost.destroy_all
+
+puts "Blog Posts deleted."
+
+puts "Creating Blog Posts..."
+
+100.times do |i|
+  BlogPost.create title: "Blog Post #{i}", content: "Hello world", published_at: Time.current
+end
+
+puts "#{BlogPost.count} Blog Pots created."
